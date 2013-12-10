f7d7dca9cf4f70dc60abc138e6ef6e36a6c522c3
andreasmay <fh@mayandreas.de>
2013-12-10 11:34:02 +0100

    fail

--- src/test/java/de/andreas/da/lab3/TestCalculator.java
+++ src/test/java/de/andreas/da/lab3/TestCalculator.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorTests {
+public class TestCalculator {
 	private Calculator calc;
 
 	@Before
